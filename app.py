5c15d04d3384d37935765c36fc4138227842dda7
조윤서 <124692313+chobbong@users.noreply.github.com>
2024-04-04 10:47:33 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -79,9 +79,14 @@ student_color_scores_df_uploaded = pd.DataFrame(student_color_scores_new).T
 # 각 학생별로 점수가 높은 순으로 색깔을 정렬합니다.
 sorted_scores_df_uploaded = student_color_scores_df_uploaded.apply(lambda x: x.sort_values(ascending=False), axis=1)
 
+#--------------------------------------------표시------------------------------------------------
+st.markdown("""
+   ##### (유)Edupia 홍쌤 색채심리 연구소
+""")
 # Streamlit 앱의 제목 설정
-st.title('휴먼컬러/정서 행동 특성 검사 결과')
-
+st.markdown("""
+## 휴먼컬러/정서 행동 특성 검사 결과
+""")
 # DataFrame에서 학생 이름 목록 추출
 students_list = sorted_scores_df_uploaded.index.tolist()
 
@@ -97,8 +102,8 @@ def get_sorted_scores(student_name):
         return pd.DataFrame({"Error": ["Student name not found."]})
    
 st.markdown("""
-## 1. 휴먼컬러 결과
-### ▶ 검사자별 성격 유형 <최고점 50~최하점 10>
+### 1. 휴먼컬러 결과
+#### 검사자별 성격 유형 <최고점 50~최하점 10>
 """)
 
 # 결과 표시
@@ -106,12 +111,12 @@ student_scores_df = get_sorted_scores(selected_student)
 st.write(student_scores_df)
 
 st.write("""
-### 최고 점수와 최하 점수 차이 분석 기준
+#### 최고 점수와 최하 점수 차이 분석 기준
 - **7점 미만의 차이:** 소극적인 참여 또는 진실성 결여 가능성이 있음
 - **7점~14점 차이:** 다양성, 수용성이 높으나 주관성, 가치관 정립이 낮을 수 있음
 - **15점 이상 차이:** 또렷한 성격적 특성을 가지고 있음
 
-### 점수 배점별 해석 기준
+#### 점수 배점별 해석 기준
 - **최고 점수 컬러:** 검사자의 성격적 고유한 주요 특성
 - **중간 점수 컬러:** 검사자의 성격적 중간 에너지 사용
 - **최하 점수 컬러:** 검사자의 소극적 성격 특성
@@ -129,7 +134,7 @@ def main():
     
         # Create the linear plot
         plt.figure(figsize=(10, 6))
-        plt.plot(colors, scores, marker='o', linestyle='-', color='b')
+        plt.plot(colors, scores, marker='o', linestyle='-', color='orange')
     
         # Annotate each point with its score
         for i, score in enumerate(scores):
@@ -140,49 +145,53 @@ def main():
         plt.xlabel('Subject')
         plt.ylabel('Score')
         plt.grid(True)
+        
+         # 설정한 y축 범위와 눈금 간격을 적용
+        plt.ylim(10, 50)  # y축의 최소값과 최대값 설정
+        plt.yticks(range(10, 51, 5))  # y축에 표시될 눈금 설정
     
         # Display the plot in Streamlit
         st.pyplot(plt)
         
 if __name__ == "__main__":
     main()
-color_info_dict_full = {'purple': {'요약': '독창성과 신비로운 보라',
+color_info_dict_full = {'보라': {'요약': '독창성과 신비로운 보라',
   '성격상 강점': '자신을 특별하다고 여기며 독특한 매력을 인정받고 싶어함.\n정신적이고 신비로운 것에 끌리며 탐구력을 보임.\n믿음과 신뢰를 구축한 사람은 끝까지 함께하며 책임감을 가짐.\n변화와 안정감 두 개의 마음을 가짐.\n관계 중시.\n자신이 선택한 것에 책임감을 가짐.',
   '성격상 약점': '양가감정이 있어 변덕스럽거나 감정기복이 있음.\n정신적인 것을 너무 추구하면 고독, 불안, 우울이 있을 수 있음.\n정체성의 혼란이 있을 수 있음.\n타인과 구별되는 우월감을 가짐.',
   '추천 직업': '행위예술가, 연극배우, 수필가, 극작가, 시인, 종교인, 상담사, 치료사, 사진가, 건축가, 실내디자이너, 일러스트레이터, 문학 연구자, 사회학자'},
- 'pink': {'요약': '상냥하며 매력적인 유형',
+ '분홍': {'요약': '상냥하며 매력적인 유형',
   '성격상 강점': '온화하고 따뜻한 성격의 소유자.\n타인의 감정에 대해 민감하며 공감능력이 뛰어남.\n감수성이 풍부하며 애교가 많음.\n타인이 나를 어떻게 생각하는지에 대한 관심이 많음.\n보호하거나 보호받고 싶어함.\n협조적이며 순응적인 성격.',
   '성격상 약점': '즉흥적 감정으로 표현하는 경우가 있어 신중함이 적음\n사랑, 관심 받고 싶은 마음이 커서 사람과의 관계에서 자주 스트레스를 받음\n독립심이 적고 의존적인 성향임\n허영심이 있기도 하며 무책임함이 있을 수 있음',
   '추천 직업': '예술가, 음악가, 공예가, 마술사, 댄서, 모델, 반려견 사업, 애완견 미용, 플로리스트, 호텔리어, 카페, 웨딩플래너, 메이크업아티스트, 간호사, 보육교사, \n어린이 관련 직업'},
- 'green': {'요약': '예의바르며 성실한 안정형',
+ '초록': {'요약': '예의바르며 성실한 안정형',
   '성격상 강점': '예의가 바르고 겸손한 성향\n맡겨진 일에 좋은 결과를 노력하지만 자신의 여가도 중요시 함\n관계 중심적으로 갈등과 싸움을 싫어해 중립적임\n이해와 배려심이 많아 친구들에게 인기가 많으며 친밀한 관계를 형성함',
   '성격상 약점': '소극적이며 내성적임\n게으른 면이 있을 수 있음\n소심해 스트레스를 많이 받음\n적극적이고 과감한 상황에 용기 부족',
   '추천 직업': '공무원, 교사, 공무직, 금융업, 감리사, 세무사, IT개발자, 전문기능인, 직업상담사, 임상병리사, 재활치료사, 동물훈련사, 1차 산업, 환경운동가, 요식업'},
- 'red': {'요약': '열정 카리스마 리더형',
+ '빨강': {'요약': '열정 카리스마 리더형',
   '성격상 강점': '적극적이고 주도적임\n책임감이 강하고 승부욕이 있음\n확신을 갖는 일에 소신껏 밀어부침\n실행력을 동반한 행동파\n뒤끝 없으며 과감함\n자신감과 자의식이 강함',
   '성격상 약점': '성격이 급하고 충동적임\n이기적이고 자의식이 강해 자신의 뜻대로 안되면 화가 남\n원리원칙에서 벗어날 경우 상대를 불신함\n폭발적인 성격으로 감정 다스림이 필요함',
   '추천 직업': '군인, 경찰, 판사, 검사, 검사, 흉부외과, 정형외과, 마케팅 전문가, 경영자, 사업가, 창업가, 정치인, 운동선수, 기업가, 모델'},
- 'yellow': {'요약': '자존감이 높은 성장 마인드',
+ '노랑': {'요약': '자존감이 높은 성장 마인드',
   '성격상 강점': '새로운 것에 관심이 많고 호기심도 완성함\n변화를 두려워하지않으며 도전의식이 강함\n사교적인 성격으로 친구가 많으며 금새 사귀기도 함\n표현력이 풍부하고 마음이 따뜻해 인기가 많음\n긍정적인 성격으로 갈등에서도 빠르데 회복함',
   '성격상 약점': '다양성 추구로 금새 흥미를 잃음\n얇고넓은 지식이 있음\n질투를 쉽게 느낌\n이기고 싶은 욕구\n집중력, 진득함과 타인을 배려하는 태도가 필요함',
   '추천 직업': '인플루언스, 전문 블로거, 에디터, 잡지, 광고, 언론, 방송인, 기상 캐스터, 기자, 리포터, 패션 디렉터, 머쳔다이저, 베이커리, 캐릭터 디자인'},
- 'orange': {'요약': '활력 넘치는 낙천가 유형',
+ '주황': {'요약': '활력 넘치는 낙천가 유형',
   '성격상 강점': '재미, 흥미, 활기를 추구함\n외부 세계에 관심이 많으며 역동적인 에너지가 충만함\n창의성과 아이디어가 많아 친구들에게 인기가 많음\n순발력과 임기응변이 뛰어남',
   '성격상 약점': '집중력이 매우 짧고 산만함\n경솔하거나 무모한 태도가 있음\n책임감이 적어 맡은 일을 완수하지 못함\n자신만 주목받고 싶은 욕구가 있어 주목받는 사람에게 질투를 느낌\n끈기과 인내가 필요하며 계획을 세워 실천하는 책임감 필요',
   '추천 직업': '연예인, 개그맨, 댄서, 마술사, 메이크업 아티스트, 인터넷 방송인, 응원단, 예술가, 영업, 마케팅, 세일즈맨, 홍보담당자, 레저활동 전문가, 여행, 숙박업, \n스포츠 매니저, 운동선수'},
- 'brown': {'요약': '침착한 속 싶은 헌신가',
+ '갈색': {'요약': '침착한 속 싶은 헌신가',
   '성격상 강점': '마음이 따뜻하고 타인에게 도움이 되고 싶어하며 배려하는 성격\n마음에 든 사람과 가족에게 헌신함\n주어진 일에 책임감과 성실성이 있음\n인내와 끈기가 강함',
   '성격상 약점': '변화를 싫어하고 느림\n타인을 과하게 배려해 손해를 봄\n고집스럽고 보수적인 성격\n촌스럽거나 답답해 보일 수 있음\n변화를 두려워하지말고 도전하는 자세 필요',
   '추천 직업': '도서관 사서, 문서 작성가, 출판사 편집자, 고고학자, 간호사, 교사, 공무원, 비서, 사회복지사, 장애인 복지사, 상담사, 요리사, 세무사, 감리사, 농업계 연구원, \n가구 디자이너, 현모양처'},
- 'black': {'요약': '조용한 목표 추구형',
+ '검정': {'요약': '조용한 목표 추구형',
   '성격상 강점': '타인에게 의존하지 않고 스스로 해결하는 유형\n자기 주도성이 강하며 조용히 목표를 세우고 실천해 나감\n자기 생각과 감정에 집중하며 자신만의 생활방식이 있음\n관심분야에 높은 집중력을 보임',
   '성격상 약점': '표현능력이 부족하며 사교성이 적음\n관심분야 외 소극적인 태도\n타인의 행동이나 생각을 비판적으로 바라보는 경향이 있음\n자신의 생각에 대한 고집이 강함\n소통, 공감능력과 사교성 필요',
   '추천 직업': '기업가, 정치인, 로비스트, 딜러, 연구원, 엔지니어, 과학자'},
- 'blue': {'요약': '스마트한 논리와 분석형',
+ '파랑': {'요약': '스마트한 논리와 분석형',
   '성격상 강점': '분석적이고 이성적임\n학문탐구적 성향\n따뜻한 마음으로 봉사에 대한 관심\n성실하며 계획적으로 맡겨진 일을 끈기있게 해 냄\n책임감이 강하며 타인에게 신뢰감을 줌',
   '성격상 약점': '신념이 확고해 고집이 세고 굽히려 하지 않음\n자기 주장을 펼쳐 독선적일 때가 있음\n규칙에 반하면 불편함을 느낌\n군중 속의 외로움을 느껴 심할 경우 우울감이 있을 수 있음',
   '추천 직업': '조력가, 교수, 교사, 뇌과학자, 내과의사, 한의사, 연구원, 역사학자, 변호사, 회계사, 컨설턴트, 금융전문가, 공무원, 엔지니어, 경찰, 출판업, 수필가, 비평가, \n물리학자, 수학자, 철학자, 사상가'},
- 'white': {'요약': '다재다능한 완벽 추구형',
+ '하양': {'요약': '다재다능한 완벽 추구형',
   '성격상 강점': '자신과 타인에게 엄격한 기준을 가지고 있으며 완벽을 추구함\n순수하고 정직함을 중요시하며 진실한 성향\n다양한 분야에 재능을 보이며 높은 목표를 가짐\n깔끔한 성격으로 정리정돈이 되어 있어야 마음이 놓임',
   '성격상 약점': '높은 목표와 완벽한 결과를 위해 지칠 수 있음\n실수를 용인하는 태도 필요\n문제해결 중심으로 감성과 공감 부족\n협력과 조화가 필요',
   '추천 직업': '검사, 판사, 법무사, 법학교수, 의사, 약사, 검안관, 보건복지 관련 공무원, 섬세한 기술의 엔지니어, 공학 기술자, 과학자, 경제학자, 금융전문가, 회계사'}}
@@ -214,23 +223,52 @@ st.write(f'<div class="custom-box">요약 : {color_info}</div>',unsafe_allow_htm
 
 # 성격상 강점 및 약점에서 줄바꿈 문자를 쉼표로 교체하여 출력
 
-character_strengths = highest_score_color_info['성격상 강점'].replace('\n', ', ')
-character_weaknesses = highest_score_color_info['성격상 약점'].replace('\n', ', ')
+character_strengths = highest_score_color_info['성격상 강점']
+character_weaknesses = highest_score_color_info['성격상 약점']
 
 
 st.markdown('<br>', unsafe_allow_html=True)
-st.write(f'<div class="custom-box">성격상 강점</div>',unsafe_allow_html=True)
-st.write(f"{character_strengths}")
+st.write(f'<div class="custom-box">성격성강점</div>',unsafe_allow_html=True)
+st.text(f"{character_strengths}")
 
 st.markdown('<br>', unsafe_allow_html=True)
-st.write(f'<div class="custom-box">성격상 약점</div>',unsafe_allow_html=True)
-st.write(f"{character_weaknesses}")
+st.write(f'<div class="custom-box">성격성약점</div>',unsafe_allow_html=True)
+st.text(f"{character_weaknesses}")
 
 # 추천 직업 출력
 st.markdown('<br>', unsafe_allow_html=True)
 st.write(f'<div class="custom-box">추천 직업</div>',unsafe_allow_html=True)
 st.write(f"{highest_score_color_info['추천 직업']}")
 
+st.markdown("""
+#### 컬러별 특성
+""")
+
+color_info_1 = {
+
+    "하양": ["성실한 책임 완수", "완벽성과 다재다능", "정직과 청렴", "예민, 민감, 스트레스 취약"],
+    "노랑": ["진취적인 성향", "밝고 명랑한 관계 지향", "호기심과 탐구심", "질투심, 비판적, 책임감 부족"],
+    "주황": ["활발하고 에너지 넘침", "독립적, 자립적, 자유분방", "용감함, 도전적, 추진력", "산만함, 집중력과 인내심 부족"],
+    "분홍": ["다정함, 상냥함, 친절함", "섬세하고 감성적, 예술성", "화합, 협조적, 순응적", "불안감, 의존적, 독립심 부족"],
+    "빨강": ["열정, 용감, 도전, 자신감", "책임감, 솔직함, 의리", "직설적, 과감함, 다양한 경험", "감정적, 충동성, 분노, 공격성"],
+   
+}
+# DataFrame으로 변환
+df_color = pd.DataFrame(color_info_1, index=["1", "2", "3", "4"])
+
+st.table(df_color)
+
+color_info_2 = { 
+    "초록": ["책임감과 성실함", "예의바른, 겸손함", "이해심, 평화적, 갈등 해결", "이기심, 소심함, 소극적"],
+    "파랑": ["탐구적, 학구적, 지성적", "분석력, 치밀함, 계획성", "책임감, 규칙과 질서", "고독, 우울, 고집, 독단"],
+    "갈색": ["안정감과 평온함", "인내, 끈기, 참음", "헌신, 봉사, 내성적, 보수적", "융통성과 이해력 부족, 지루함"],
+    "보라": ["신비롭고 매력적, 통찰력", "독착성, 창의, 예술, 자유로움", "독립적, 자존심이 강함", "우월감, 고집, 차별"],
+    "검정": ["조용함, 독립성, 내성적", "깊은 생각, 통찰력", "비밀이 많은, 현실적인", "우울, 절망, 고독, 내면을 숨김"]
+}       
+
+# DataFrame으로 변환
+df_color_2 = pd.DataFrame(color_info_2, index=["1", "2", "3", "4"])
+st.table(df_color_2)
 
 # 'character' 시트의 Emotion 정보를 사용하여 문제 ID를 색깔별로 그룹화합니다.
 emotion_groups_new = character_sheet[['Question', 'Emotion']].set_index('Question')['Emotion'].to_dict()
@@ -247,74 +285,162 @@ student_emotion_scores_df_uploaded = pd.DataFrame(student_emotion_scores_new).T
 # 각 학생별로 점수가 높은 순으로 색깔을 정렬합니다.
 sorted_scores_df_uploaded_emotion = student_emotion_scores_df_uploaded.apply(lambda x: x.sort_values(ascending=False), axis=1)
 
+
+#--------------------------------------------표시------------------------------------------------
 st.markdown("""
-## 2. 정서 행동 특성 검사 결과
+### 2. 정서 행동 특성 검사 결과
 """)
-# 데이터 프레임을 생성합니다.
+
+st.markdown("""
+#### 정서 행동 특성 설명
+""")
+
 data = {
     'NO': [1, 2, 3, 4, 5, 6],
-    '구분': ['공감능력', '자기효능감', '책임감', '사교성', '자기표현능력', '자존감'],
+    '구분': ['책임감', '사교성', '자기표현', '자존감', '공감능력', '자기효능감'],
     '설명': [
-        '타인의 감정을 이해하고 공유하는 능력',
-        '어떤 상황에서 적절한 행동을 할 수 있다는 기대와 신념',
         '맡아서 해야 할 임무나 의무를 중요하게 여기는 마음과 태도',
         '타인과 원만하게 상호작용 하는 능력',
         '자신의 마음을 정직하게 표현 할 수 있는 사회적 능력',
-        '자신을 소중하고 가치있는 존재로 여기는 마음'
+        '자신을 소중하고 가치있는 존재로 여기는 마음',
+        '타인의 감정을 이해하고 공유하는 능력',
+        '어떤 상황에서 적절한 행동을 할 수 있다는 기대와 신념',
     ]
 }
 
 df = pd.DataFrame(data)
 # 스트림릿에 표를 나타냅니다.
-st.table(df.set_index('NO'))
-st.write("(최고점 50점~최하점 10점)")
-
-# 선택된 학생의 점수를 DataFrame 형태로 표시
-def get_sorted_scores_df_by_input(student_name):
-    if student_name in sorted_scores_df_uploaded_emotion.index:
-        student_scores = sorted_scores_df_uploaded_emotion.loc[student_name].sort_values(ascending=False)
-        return pd.DataFrame(student_scores).T
-    else:
-        return pd.DataFrame({"Error": ["Student name not found."]})
+st.write(df.set_index('NO'))
 
 
+responsibility = sorted_scores_df_uploaded_emotion['책임감'][selected_student]
+sociability = sorted_scores_df_uploaded_emotion['사교성'][selected_student]
+self_expression = sorted_scores_df_uploaded_emotion['자기표현'][selected_student]
+self_esteem = sorted_scores_df_uploaded_emotion['자존감'][selected_student]
+empathy = sorted_scores_df_uploaded_emotion['공감능력'][selected_student]
+self_efficacy = sorted_scores_df_uploaded_emotion['자기효능감'][selected_student]
 
-# 결과 표시
-student_scores_df_emotion = get_sorted_scores_df_by_input(selected_student)
-student_scores_df_emotion.drop(student_scores_df_emotion.columns[0], axis=1, inplace=True)
-student_scores_df_emotion_transposed = student_scores_df_emotion.transpose()
+responsibility_ratio = responsibility / 35
+responsibility_ratio = f"{responsibility_ratio * 100:.0f}%"
 
+sociability_ratio = sociability / 35
+sociability_ratio = f"{sociability_ratio * 100:.0f}%"
 
-labels = student_scores_df_emotion.columns.values
-scores = student_scores_df_emotion.iloc[0].values
-num_vars = len(labels)
+self_expression_ratio = self_expression / 35
+self_expression_ratio = f"{self_expression_ratio * 100:.0f}%"
 
-# 각 축의 각도 계산
-angles = np.linspace(0, 2 * np.pi, num_vars, endpoint=False).tolist()
-# 루프 완성
-scores = np.concatenate((scores, [scores[0]]))
+self_esteem_ratio =self_esteem / 35
+self_esteem_ratio = f"{self_esteem_ratio * 100:.0f}%"
+
+empathy_ratio =  empathy / 35
+empathy_ratio = f"{empathy_ratio * 100:.0f}%"
+
+self_efficacy_ratio = self_efficacy / 35
+self_efficacy_ratio = f"{self_efficacy_ratio * 100:.0f}%"
+
+# 홍길동의 각 항목별 점수
+scores = {
+    '책임감': responsibility,
+    '사교성': sociability,
+    '자기표현': self_expression,
+    '자존감': self_esteem,
+    '공감능력': empathy,
+    '자기효능감': self_efficacy
+}
+
+# 각 항목별 점수 기준
+score_criteria = {
+    '책임감': {'높음': (26, 35), '보통': (16, 25), '낮음': (7, 15)},
+    '사교성': {'높음': (33, 45), '보통': (21, 32), '낮음': (9, 20)},
+    '자기표현': {'높음': (26, 35), '보통': (16, 25), '낮음': (7, 15)},
+    '자존감': {'높음': (22, 30), '보통': (14, 21), '낮음': (6, 13)},
+    '공감능력': {'높음': (22, 30), '보통': (14, 21), '낮음': (6, 13)},
+    '자기효능감': {'높음': (22, 30), '보통': (14, 21), '낮음': (6, 13)}
+}
+
+st.markdown(f"<h4 style='text-align:left; color: black;'>{selected_student} 검사 결과 </h4>", unsafe_allow_html=True)
+# 결과 데이터 초기화
+data_emotion = {
+    '구분': ['책임감', '사교성', '자기표현', '자존감', '공감능력', '자기효능감'],
+    '높음': [],
+    '보통': [],
+    '낮음': [],
+    '점수': [scores['책임감'], scores['사교성'], scores['자기표현'], scores['자존감'], scores['공감능력'], scores['자기효능감']],
+    '비중': [responsibility_ratio, sociability_ratio, self_expression_ratio, self_esteem_ratio, empathy_ratio, self_efficacy_ratio],  # 비중은 여기서는 계산하지 않고 비워둡니다.
+    '높음(기준)': ['35~25', '45~33', '35~26', '30~22', '30~22', '30~22'],
+    '보통(기준)': ['24~16', '32~21', '25~16', '21~14', '21~14', '21~14'],
+    '낮음(기준)': ['15~7', '20~9', '15~7', '13~6', '13~6', '13~6']
+}
+
+# 결과 데이터 초기화
+data_emotion_2 = {
+    'NO': [1, 2, 3, 4, 5, 6],
+    '구분': ['책임감', '사교성', '자기표현', '자존감', '공감능력', '자기효능감'],
+    '점수': [scores['책임감'], scores['사교성'], scores['자기표현'], scores['자존감'], scores['공감능력'], scores['자기효능감']],
+    '비중': [responsibility_ratio, sociability_ratio, self_expression_ratio, self_esteem_ratio, empathy_ratio, self_efficacy_ratio],  # 비중은 여기서는 계산하지 않고 비워둡니다.
+
+}
+df_2 = pd.DataFrame(data_emotion_2)
+# 각 항목별 점수를 기준에 따라 분류
+for category, score in scores.items():
+    for classification, (min_score, max_score) in score_criteria[category].items():
+        if min_score <= score <= max_score:
+            data_emotion[classification].append('O')
+        else:
+            data_emotion[classification].append('')
+
+df = pd.DataFrame(data_emotion)
+st.write(df.set_index('구분'))
+
+score_ratio = {
+     '책임감': responsibility_ratio,
+     '사교성': sociability_ratio,
+     '자기표현': self_expression_ratio,
+     '자존감': self_esteem_ratio,
+     '공감능력': empathy_ratio,
+     '자기효능감': self_efficacy_ratio 
+}
+
+# 백분율 문자열을 실수로 변환
+scores = [float(rate[:-1]) for rate in score_ratio.values()]
+
+# 라벨
+labels = list(score_ratio.keys())
+
+# 각 라벨의 각도 계산
+angles = np.linspace(0, 2 * np.pi, len(labels), endpoint=False).tolist()
+
+# 그래프를 닫기 위해 시작점으로 돌아가는 점수와 각도 추가
+scores += scores[:1]
 angles += angles[:1]
 
-fig, ax = plt.subplots(figsize=(8, 8), subplot_kw=dict(polar=True))
-ax.fill(angles, scores, color='red', alpha=0.25)
-ax.plot(angles, scores, color='red', linewidth=2)
+# 방사형 그래프 생성
+fig, ax = plt.subplots(figsize=(5, 5), subplot_kw=dict(polar=True))
+ax.fill(angles, scores, alpha=0)
+ax.plot(angles, scores, color='blue', linewidth=1)
 
-# 각 점수 라벨 그리기
-ax.set_xticks(angles[:-1])
-ax.set_xticklabels(labels, fontsize=20)
-
-# 각 점수에 대해 주석 달기
+# Annotate each point with its score
 for label, angle, score in zip(labels, angles, scores):
-    ax.text(angle, score, str(score), ha='center', va='center', fontsize=20, color='blue')
+    ax.text(angle, score, f"{int(score)}%", ha='center', va='center', fontsize=15, color='blue')
 
-ax.set_yticklabels([])
+# 라벨 위치 설정
+ax.set_thetagrids(np.degrees(angles[:-1]), labels)
 
-# Streamlit에 그래프 표시
-col1, col2, col3 = st.columns([3, 7, 3]) # 화면을 세 부분으로 나눔
+# 원을 10%부터 100%까지 설정
+yticks = np.linspace(10, 100, 10)  # 0.1부터 1.0까지 10개의 틱 생성
+ax.set_yticks(yticks)  # y축 틱 설정
 
-with col1: 
-    st.write(student_scores_df_emotion_transposed )
+col1, col2, col3 = st.columns([5, 7, 2])
 
-with col2: # 가운데 컬럼에 그래프를 표시
-    st.pyplot(fig)
+with col1:
+    st.write(df_2.set_index('NO'))
 
+with col2:
+    st.pyplot(plt)
+
+    
+st.markdown(f"<br>",unsafe_allow_html=True)
+st.markdown(f"<br>",unsafe_allow_html=True)
+st.markdown("""
+   ##### (유)Edupia 홍쌤 색채심리 연구소
+""")
